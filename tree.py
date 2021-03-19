4ab5e1457c204cd07584cd9f16d1cc2ee1ce28ad
Hlibisev <64321152+Hlibisev@users.noreply.github.com>
2021-03-19 09:19:31 +0300

    adding test, adding new priori partition

--- tree.py
+++ tree.py
@@ -26,7 +26,7 @@ class DecisionTreeNode:
 
 
 class BayesianDecisionTree:
-    def __init__(self,  max_depth: int = 20, min_samples_leaf: int = 1, partition_prior=0.9, prior=[10, 10, 10, 100]):
+    def __init__(self,  max_depth: int = 20, min_samples_leaf: int = 1, partition_prior=(0.9, 30), prior=(10, 10, 10, 100)):
         """
         :param max_depth: max depth of tree
         :param min_samples_leaf: how many samples should be at leaf to avoid separation
@@ -56,8 +56,23 @@ class BayesianDecisionTree:
                     sep = X[node.ind, split_dim] < split_value
                     left_ind, right_ind = node.ind[sep], node.ind[~sep]
 
+                    if len(left_ind) == 1 or len(right_ind) == 1:
+                        node.posterior = self.get_posterior(node, y[node.ind])
+                        continue
+
+                    posterior_left = self.get_posterior(node, y[left_ind])
+                    posterior_right = self.get_posterior(node, y[right_ind])
+                    # posterior_left = tuple([y[left_ind].mean()]) + posterior_left[1:]
+                    # posterior_right = tuple([y[right_ind].mean()]) + posterior_right[1:]
+
+                    # left, right = self.make_nodes(node, split_dim, split_value, left_ind,
+                                                  # right_ind, node.prior, node.prior)
+
+                    prior_left = self.set_prior(y, node)
+                    prior_right = self.set_prior(y, node)
+
                     left, right = self.make_nodes(node, split_dim, split_value, left_ind,
-                                                  right_ind, node.prior, node.prior)
+                                                  right_ind, prior_left, prior_right)
 
                     new_nodes.add(left)
                     new_nodes.add(right)
@@ -87,18 +102,17 @@ class BayesianDecisionTree:
             y_sorted_by_feature = y[X_sorted_indexes]
 
             indexes_of_unique = np.unique(X_f_sorted_by_feature, return_index=True)[1][1:]
-            n_splits = len(indexes_of_unique)
 
             posterior_left, posterior_right = self.get_posteriors_for_split_node(node, y_sorted_by_feature, indexes_of_unique)
-            log_p_data_after_split = self.get_log_p_data_after_split(node, posterior_left, posterior_right,
-                                                                     n_splits, n_features, len(X_sorted_indexes))
+            log_p_data_after_split = self.get_log_p_data_after_split(node, posterior_left, posterior_right, len(X_sorted_indexes))
             index_max = log_p_data_after_split.argmax()
 
             if log_p_data_after_split[index_max] > best_log_data:
 
                 # remember new best split
                 best_log_data = log_p_data_after_split[index_max]
-                split_value = X_f_sorted_by_feature[indexes_of_unique[index_max]]
+                # split_value = X_f_sorted_by_feature[indexes_of_unique[index_max] - 1: indexes_of_unique[index_max]].mean()
+                split_value = X_f_sorted_by_feature[indexes_of_unique[index_max: index_max + 1]].mean()
                 split_dim = feature
 
         return split_dim, split_value
@@ -149,19 +163,27 @@ class BayesianDecisionTree:
         n = len(y)
         mean_y, variance_y_mul_n = self._comute_mean_and_var(y)
         mu_post, kappa_post, alpha_post, beta_post = self._get_posterior(node, n, mean_y, variance_y_mul_n)
-        log_p_prior = np.log(1 - self.partition_prior ** (1 + node.depth))
+        log_p_prior = self.get_log_p_prior(node.depth, n)
         log_p_data = self._get_log_p_data(node, alpha_post, beta_post, kappa_post, n)
 
         return log_p_prior + log_p_data
 
-    def get_log_p_data_after_split(self, node: DecisionTreeNode, posterior_for_left_node, posterior_for_right_node, n_splits, n_dim, n):
+    def get_log_p_prior(self, depth, n1, n2=None):
+        depth_penalty = self.partition_prior[0] ** (1 + depth)
+        samples_penalty1 = 2 * np.arctan(n1 / self.partition_prior[1]) / np.pi
+        if n2 is not None:
+            samples_penalty2 = 2 * np.arctan(n2 / self.partition_prior[1]) / np.pi
+            return np.log(depth_penalty * samples_penalty1 * samples_penalty2)
+        return np.log(depth_penalty * samples_penalty1)
+
+    def get_log_p_data_after_split(self, node: DecisionTreeNode, posterior_for_left_node, posterior_for_right_node, n):
         n1 = np.arange(1, n)
         n2 = n - n1
 
         mu1, kappa1, alpha1, beta1 = posterior_for_left_node
         mu2, kappa2, alpha2, beta2 = posterior_for_right_node
 
-        log_p_prior = np.log(self.partition_prior ** (1 + node.depth) / (n_splits * n_dim))
+        log_p_prior = self.get_log_p_prior(node.depth + 1, n1, n2)
         log_p_data1 = self._get_log_p_data(node, alpha1, beta1, kappa1, n1)
         log_p_data2 = self._get_log_p_data(node, alpha2, beta2, kappa2, n2)
 
@@ -200,18 +222,38 @@ class BayesianDecisionTree:
         variance_y_mul_n = ((y - mean_y) ** 2).sum()
         return mean_y, variance_y_mul_n
 
+    def set_prior(self, y, node):
+        mu = (y.mean() + node.prior[0])/2
+        sd_prior = y.std() / 10
+        prior_obs = 1
+        kappa = prior_obs
+        alpha = prior_obs / 2
+        var_prior = sd_prior ** 2
+        tau_prior = 1 / var_prior
+        beta = alpha / tau_prior
+        return mu, kappa, alpha, beta
 
 from sklearn.metrics import mean_absolute_error as mae
 
 if __name__ == "__main__":
     # [ 2.33333333 23.          2.33333333 23.          0.95      ]
-    # X = np.array([[1, 3], [3, 4], [5, 0.6], [7, 8], [9, 0.10]])
-    # y = X[:, 0] * X[:, 1]
+    n_data = 300
 
-    X = np.random.rand(2000, 5) * 10
-    y = X[:, 1] * X[:, 2] - X[:, 3] + np.sin(X[:, 4])
+    X = np.linspace(-1, 3, n_data).reshape(-1, 1)
+    y = X[:, 0] ** 2 - X[:, 0] + np.random.rand(n_data) + np.cos(X[:, 0])
 
-    prior = [10, 1, 1, 1]
+    mu = y.mean()
+    sd_prior = y.std()
+    prior_obs = 2
+    kappa = prior_obs
+    alpha = prior_obs / 2
+    var_prior = sd_prior ** 2
+    tau_prior = 1 / var_prior
+    beta = alpha / tau_prior
+    partition = (0.9, 10)
+
+    prior = [mu, kappa, alpha, beta]
+
     tree = BayesianDecisionTree()
     tree.fit(X, y)
     print(mae(y, tree.predict(X)))
